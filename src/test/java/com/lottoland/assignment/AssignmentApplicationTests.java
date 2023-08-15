b8c28c735c121c3b146e8f6f966547d82396ceeb
lukegom94 <83473675+lukegom94@users.noreply.github.com>
2023-08-15 10:34:42 +0200

    Project Structure
    Generation of Spring project with dependencies in Spring Initializr

---
+++ src/test/java/com/lottoland/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lottoland.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
